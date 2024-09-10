a2693bee49de0ff2a808f9639c65aad013455ee4
Thivakaran-R <thivakaranramu10@gmail.com>
2024-09-10 10:55:29 +0530

    new

--- src/Components/Propsdrilling/Propsdrilling.jsx
+++ src/Components/Propsdrilling/Propsdrilling.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Child1 from './Child1'
 
 const Propsdrilling = () => {
-    const text= "Hi, Learning React"
+    const text= "Hi, Learning React."
   return (
     <div>
         <Child1 text={text}/>
