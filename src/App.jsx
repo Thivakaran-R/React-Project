1593df7f5b01290c277512f2353c6ae94b0ac826
Thivakaran-R <thivakaranramu10@gmail.com>
2024-09-10 12:46:38 +0530

    toggle button dark and light theme

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import Child2 from "./Components/Propsdrilling/Child2";
 import Propsdrilling from "./Components/Propsdrilling/Propsdrilling";
+import Toggle from "./Components/Toggle-Switch/Toggle";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return <>
+    <Toggle/>
     <Propsdrilling/>
   </>;
 }
--- src/Components/Toggle-Switch/Toggle.jsx
+++ src/Components/Toggle-Switch/Toggle.jsx
@@ -1,13 +1,24 @@
 import React, { useState } from 'react'
+import "./Toggle.css"
 
 const Toggle = () => {
     const [isDarktheme,setisDarktheme] =useState(false);
     const toggleTheme =()=>{
         setisDarktheme(!isDarktheme);
+        if (isDarktheme) {
+            document.body.classList.remove('dark');
+          } else {
+            document.body.classList.add('dark');
+          }
     }
   return (
     <div>
-        <
+        <div className='container'>
+            <h1>{isDarktheme ? "Dark Theme" : "Light Theme"}</h1>
+            <button onClick={toggleTheme}className='togglebutton'>
+                {isDarktheme ? "light Theme":"Dark Theme"}
+            </button>
+        </div>
     </div>
   )
 }
